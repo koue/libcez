c2f5dcc37190a26dc80c26a0e164cccc7e1b3cbc
Nikola Kolev <koue@chaosophia.net>
2018-12-20 04:58:02 +0000

    Update cmake module. Remove config search , add queue search

--- cmake/FindLibcez.cmake
+++ cmake/FindLibcez.cmake
@@ -31,22 +31,20 @@
 #
 # ::
 #
-#  LIBCEZ_CONFIG_INCLUDE_DIR - libcez config include directory
 #  LIBCEZ_FOSSIL_INCLUDE_DIR - libcez fossil include directory
 #  LIBCEZ_KV_INCLUDE_DIR - libcez key-value include directory
 #  LIBCEZ_MISC_INCLUDE_DIR - libcez misc include directory
 #  LIBCEZ_PRAYER_INCLUDE_DIR - libcez prayer include directory
+#  LIBCEZ_QUEUE_INCLUDE_DIR - libcez queue include directory
 #
-#  LIBCEZ_CONFIG_LIBRARIES - libcez config library
 #  LIBCEZ_FOSSIL_LIBRARIES - libcez fossil library
 #  LIBCEZ_KV_LIBRARIES - libcez key-value library
 #  LIBCEZ_MISC_LIBRARIES - libcez misc library
 #  LIBCEZ_PRAYER_LIBRARIES - libcez prayer library
+#  LIBCEZ_QUEUE_LIBRARIES - libcez queue library
 #
 #  LIBCEZ_FOUND - libcez found
 
-# Look for libcez config header file.
-find_path(LIBCEZ_CONFIG_INCLUDE_DIR NAMES cez_config.h)
 # Look for libcez fossil header file.
 find_path(LIBCEZ_FOSSIL_INCLUDE_DIR NAMES cez_fossil.h)
 # Look for libcez key-value header file.
@@ -55,9 +53,9 @@ find_path(LIBCEZ_FOSSIL_INCLUDE_DIR NAMES cez_kv.h)
 find_path(LIBCEZ_MISC_INCLUDE_DIR NAMES cez_misc.h)
 # Look for libcez prayer header file.
 find_path(LIBCEZ_MISC_INCLUDE_DIR NAMES cez_prayer.h)
+# Look for libcez queue header file.
+find_path(LIBCEZ_MISC_INCLUDE_DIR NAMES cez_queue.h)
 
-# Look for libcez config library
-find_library(LIBCEZ_CONFIG_LIBRARIES NAMES cezconfig)
 # Look for libcez fossil library
 find_library(LIBCEZ_FOSSIL_LIBRARIES NAMES cezfossil)
 # Look for libcez key-value library
@@ -66,19 +64,21 @@ find_library(LIBCEZ_FOSSIL_LIBRARIES NAMES cezkv)
 find_library(LIBCEZ_MISC_LIBRARIES NAMES cezmisc)
 # Look for libcez prayer library
 find_library(LIBCEZ_MISC_LIBRARIES NAMES cezprayer)
+# Look for libcez queue library
+find_library(LIBCEZ_MISC_LIBRARIES NAMES cezqueue)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Libcez DEFAULT_MSG
-			LIBCEZ_CONFIG_INCLUDE_DIR LIBCEZ_CONFIG_LIBRARIES
 			LIBCEZ_FOSSIL_INCLUDE_DIR LIBCEZ_FOSSIL_LIBRARIES
 			LIBCEZ_KV_INCLUDE_DIR LIBCEZ_KV_LIBRARIES
 			LIBCEZ_MISC_INCLUDE_DIR LIBCEZ_MISC_LIBRARIES
 			LIBCEZ_PRAYER_INCLUDE_DIR LIBCEZ_PRAYER_LIBRARIES
+			LIBCEZ_QUEUE_INCLUDE_DIR LIBCEZ_QUEUE_LIBRARIES
 )
 
-mark_as_advanced(LIBCEZ_CONFIG_INCLUDE_DIR LIBCEZ_CONFIG_LIBRARIES
-		LIBCEZ_FOSSIL_INCLUDE_DIR LIBCEZ_FOSSIL_LIBRARIES
+mark_as_advanced(LIBCEZ_FOSSIL_INCLUDE_DIR LIBCEZ_FOSSIL_LIBRARIES
 		LIBCEZ_KV_INCLUDE_DIR LIBCEZ_KV_LIBRARIES
 		LIBCEZ_MISC_INCLUDE_DIR LIBCEZ_MISC_LIBRARIES
 		LIBCEZ_PRAYER_INCLUDE_DIR LIBCEZ_PRAYER_LIBRARIES
+		LIBCEZ_QUEUE_INCLUDE_DIR LIBCEZ_QUEUE_LIBRARIES
 )
