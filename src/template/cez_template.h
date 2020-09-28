5666435e0afb40eb3099b41536d958e4cb0d57bd
Nikola Kolev <koue@chaosophia.net>
2020-09-28 23:52:07 +0000

    Update Makefile.cipier

--- src/template/cez_template.h
+++ src/template/cez_template.h
@@ -31,24 +31,6 @@
 #ifndef _CEZ_TEMPLATE_H
 #define _CEZ_TEMPLATE_H
 
-/*
-#include <assert.h>
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <grp.h>
-#include <netdb.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <syslog.h>
-#include <pwd.h>
-#include <unistd.h>
-*/
 #include <assert.h>
 #include <sys/stat.h>
 
